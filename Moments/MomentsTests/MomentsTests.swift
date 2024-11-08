1ea11107c3c014d88f26e3875999e9ef86b07ecd
Guoruize <yimao009@gmail.com>
2024-11-09 00:47:16 +0800

    Create a new iOS project.

---
+++ Moments/MomentsTests/MomentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MomentsTests.swift
+//  MomentsTests
+//
+//  Created by GRZ on 2024/11/9.
+//
+
+import Testing
+@testable import Moments
+
+struct MomentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
